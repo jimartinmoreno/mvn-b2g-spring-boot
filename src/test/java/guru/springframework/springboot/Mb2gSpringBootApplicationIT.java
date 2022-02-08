68d36fe0cfce2eaf9c414e9ee2a5fb70205bb3fe
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2022-02-08 14:02:31 +0100

    Initial Release

--- src/test/java/guru/springframework/springboot/Mb2gSpringBootApplicationIT.java
+++ src/test/java/guru/springframework/springboot/Mb2gSpringBootApplicationIT.java
@@ -2,20 +2,22 @@ package guru.springframework.springboot;
 
 import guru.springframework.springboot.controllers.ExampleController;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class Mb2gSpringBootApplicationTests {
+public class Mb2gSpringBootApplicationIT {
 
     @Autowired
     ExampleController exampleController;
 
     @Test
     public void contextLoads() {
-        System.out.println("############### UT = " + exampleController);
+        System.out.println("############### IT = " + exampleController);
         assertThat(exampleController).isNotNull();
     }
 }
